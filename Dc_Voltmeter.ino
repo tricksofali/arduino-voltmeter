db8c6296fd8dcf807e360037c9d5f7bc1f077fbc
tricksofali <93981711+tricksofali@users.noreply.github.com>
2022-02-27 22:11:30 +0330

    Add files via upload

---
+++ Dc_Voltmeter.ino
@@ -0,0 +1,44 @@
+/*
+  DC Voltmeter
+  An Arduino based on voltage divider concept
+  Youtube Channel: Tricks Of Ali
+*/
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+int analogInput = 0;
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 100000.0; // resistance of R1 (100K)
+float R2 = 10000.0; // resistance of R2 (10K)
+int value = 0;
+int a[5] = {};
+void setup() {
+  Serial.begin(9600);
+  pinMode(analogInput, INPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(2, 0);
+  lcd.print("DC VOLTMETER");
+  lcd.setCursor(0, 1);
+  lcd.print("-TRICKS OF ALI-");
+  delay(3000);
+  lcd.clear();
+
+}
+void loop() {
+  lcd.print("DC Voltmeter");
+  // read the value at analog input
+  value = analogRead(analogInput);
+  vout = (value * 5) / 1000.0;
+  vin = vout / (R2 / (R1 + R2));
+  Serial.println(vin);
+  if (vin < 0.09)
+  {
+    vin = 0.0;
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Voltage V :");
+  lcd.print(vin);
+  delay(3000);
+  lcd.clear();
+}
